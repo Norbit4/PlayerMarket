2858d20a73d58a964e47838e9f8589a0ea111320
Norbert Śleziak <norr.bitt@gmail.com>
2024-10-19 21:16:12 +0200

    Update PlayerUtils.java

--- src/main/java/pl/norbit/playermarket/utils/PlayerUtils.java
+++ src/main/java/pl/norbit/playermarket/utils/PlayerUtils.java
@@ -1,11 +1,12 @@
 package pl.norbit.playermarket.utils;
 
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import pl.norbit.playermarket.PlayerMarket;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PlayerUtils {
     private PlayerUtils() {
@@ -15,6 +16,17 @@ public class PlayerUtils {
     public static Optional<Player> getPlayer(UUID playerUUID) {
         return Optional.ofNullable(PlayerMarket.getInstance().getServer().getPlayer(playerUUID));
     }
+
+    public static OfflinePlayer getOfflinePlayer(String name) {
+        return PlayerMarket.getInstance().getServer().getOfflinePlayer(name);
+    }
+
+    public static List<String> getOfflineNames() {
+        return Arrays.stream(PlayerMarket.getInstance().getServer().getOfflinePlayers())
+                .map(OfflinePlayer::getName)
+                .collect(Collectors.toList());
+    }
+
 
     public static boolean isInventoryFull(Player p) {
         Inventory inventory = p.getInventory();
